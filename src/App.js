46662be193ec43a895a445a3c3366a6980085203
andrewhunt21 <andrewhunt21@icloud.com>
2022-03-26 18:30:41 -0700

    adding some basic styling and customizing readme

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import Timer from './Timer';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
+      <body className="App-body">
         <h1>Baby On The Way!</h1>
         <p>Estimated Due Date: 10/28/2022</p>
         <Timer date="10/28/2022" />
-      </header>
+      </body>
     </div>
   );
 }
