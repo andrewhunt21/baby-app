ddbc6e3f36d119a0f9ae758cfa9e0c21399a1d20
andrewhunt21 <andrewhunt21@icloud.com>
2022-03-26 17:24:10 -0700

    initial commit

---
+++ src/Timer.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+class Timer extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            days: 0,
+            hours: 0,
+            minutes: 0,
+            seconds: 0,
+            timeUp: false
+        }
+    }
+
+    componentDidMount() {
+        setInterval(() => {
+            let dueDate = +new Date(this.props.date);
+            let difference = dueDate - +new Date();
+
+            if (difference < 1) {
+                this.setState({ timeUp: true });
+            } else {
+                let days = Math.floor(difference / (1000 * 60 * 60 * 24));
+                let hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
+                let minutes = Math.floor((difference / (1000 * 60)) % 60);
+                let seconds = Math.floor((difference / (1000)) % 60);
+                this.setState({
+                    hours: hours > 9 ? hours : `0${hours}`,
+                    minutes: minutes > 9 ? minutes : `0${minutes}`,
+                    seconds: seconds > 9 ? seconds : `0${seconds}`,
+                    days
+                });
+            }
+        }, 1000)
+    }
+
+    render() {
+        const {days, hours, minutes, seconds, timeUp} = this.state;
+        const dayString = days > 1 ? 'days' : 'day';
+        return (
+            timeUp ?
+                <p>Baby is here in:</p>
+                :
+                <p>{ `${days} ${dayString} ${hours} hours ${minutes} minutes ${seconds} seconds` }</p>
+        );
+    }
+}
+
+export default Timer;
